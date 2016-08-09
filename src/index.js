40dbd232858c4b831c0db62d34a5628dd168b88b
Eric Hendershott <erich421@comcast.net>
2016-08-08 23:27:56 -0500

    Add youtube api and app components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAZ-cenVswEQ0_X3xxyEU-8plwgIDRPYEc';
 
 const App = () => {
-    return <div>Main Application</div>;
+    return (
+      <div>
+        <SearchBar />
+      </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
